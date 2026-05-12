f2f83e693aa65fcc8d3d620286e579a76e6fc0b0
Kota-Maeno <b223328@hiroshima-u.ac.jp>
2026-05-12 21:44:21 +0900

    ok

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -8,6 +8,8 @@ export const metadata: Metadata = {
   }
 };
 
+export const dynamic = "force-dynamic";
+
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return children;
 }
